6376d156986189e5eceff84544aa554f7d516894
Menashe <vaipheimanasheh@gmail.com>
2024-03-10 10:51:45 +0200

    matrix

--- Week2/Day5/DailyChallenges/assets/main.py
+++ Week2/Day5/DailyChallenges/assets/main.py
@@ -8,18 +8,18 @@
 # use_split_and_sort = sorted(user_input.split())
 # print(use_split_and_sort)
 
-#Challenge 2
+# Challenge 2
 # Write a function that finds the longest word in a sentence. 
 # If two or more words are found, return the first longest word.
 # Characters such as apostrophe, comma, period count as part of the word (e.g. O’Connor is 8 characters long).
 
-# def find_longest_word(str):
-#     words = str.split()
-#     longest_word = ''
-#     for word in words:
-#         if len(word) > len(longest_word):
-#             longest_word  = word
-#     return longest_word
+def find_longest_word(str):
+    words = str.split()
+    longest_word = ''
+    for word in words:
+        if len(word) > len(longest_word):
+            longest_word  = word
+    return longest_word
 
 
-# print(find_longest_word('return the first longest word in the sentence'))
+print(find_longest_word('return the first longest word from the sentence.'))
